e0e34e27235aa91e2a045c2bb213a4687fdf0fa4
Billa <adithyen1@gmail.com>
2026-05-26 00:26:11 +0530

    code rabbit bug fixed now

--- React-native/app/(tabs)/index.tsx
+++ React-native/app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
-    <SafeAreaView className="flez-1 bg-background p-5">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
           Welcome to Nativewind!
       </Text>
