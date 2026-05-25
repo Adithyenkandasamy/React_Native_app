46ac0066e72b4dba1fe00aa0a771937bbcab9aba
Billa <adithyen1@gmail.com>
2026-05-25 22:13:49 +0530

    Learned the routing in the react native

--- React-native/app/(tabs)/index.tsx
+++ React-native/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
-        i am going to fuck You Buddy
+          hi dude
       </Text>
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
         Go to Onboarding
@@ -15,6 +15,15 @@ export default function Index() {
       </Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
         Go to SignUp
+      </Link>
+      <Link href="/subscriptions/spotify">
+      Spotify Subscriptions
+      </Link>
+      <Link href = {{
+        pathname: "/subscriptions/[id]",
+        params: { id: "claude" },
+      }}>
+        Claude Max Subscription
       </Link>
     </View>
   );
