eda6c4b83b5813a8e866fc7a244d99f4111dc9cb
Billa <adithyen1@gmail.com>
2026-05-26 10:38:26 +0530

    Added the avatar and the balance card with the dataset already in the data.ts

--- React-native/app/(tabs)/index.tsx
+++ React-native/app/(tabs)/index.tsx
@@ -1,23 +1,36 @@
 import "@/global.css";
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Text,Image,View } from "react-native";
 import {styled} from "nativewind";
 import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import images from "@/constants/images";
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from 'dayjs';
 
 const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold">Home</Text>
-      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to Onboarding
-      </Link>
-      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to SignIn
-      </Link>
-      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to SignUp
-      </Link>
+        <View className="home-header">
+          <View className="home-user">
+            <Image source={images.avatar} className="home-avatar"/>
+            <Text className="home-user-name">{HOME_USER.name}</Text>
+          </View>
+          <Image source={icons.add} className="home-add-icon"></Image>
+        </View>      
+        <View className="home-balance-card">
+          <Text className="home-balance-label">Balance</Text>
+          <View className="home-balance-row">
+            <Text className="home-balance-amount">
+              {formatCurrency(HOME_BALANCE.amount)}
+            </Text>
+            <Text className="home-balance-date">
+              {dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}
+            </Text>
+          </View>
+        </View>
     </SafeAreaView>
   );
 }
+  
